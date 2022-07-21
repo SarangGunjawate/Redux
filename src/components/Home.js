d057c77b91b0aae02d4ad8e1e5b506cadeb9d191
sarang <saranggunjawate9@gmail.com>
2022-07-21 19:50:03 +0530

    initial commit

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,35 +1,32 @@
-import React from 'react'
+import React from "react";
 
 function Home(props) {
   return (
     <div>
-                <div className="add-to-cart">
-                    <img src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-add-to-cart--icon-design-png-image_4269419.jpg" />
-                </div>
-        <h1>home component</h1>
-        <div className="cart-wrapper">
-            
-                <div className="img-wrapper item">
-                    <img src="https://www.pngmart.com/files/15/Apple-iPhone-12-PNG-HD.png" />
-                </div>
-                <div className="text-wrapper item">
-                    <span>
-                        I-Phone
-                    </span>
-                    <span>
-                        Price: $1000.00
-                    </span>
-                </div>
-                <div className="btn-wrapper item">
-                    <button 
-                    onClick={
-                        ()=>{props.addToCartHandler({pice:1000,name:'i phone 11'})}
-                        }>
-                        Add To Cart</button>
-                </div>
-            </div>
+      <div className="add-to-cart">
+        <img src="https://png.pngtree.com/png-clipart/20190516/original/pngtree-add-to-cart--icon-design-png-image_4269419.jpg" />
+      </div>
+      <h1>home component</h1>
+      <div className="cart-wrapper">
+        <div className="img-wrapper item">
+          <img src="https://www.pngmart.com/files/15/Apple-iPhone-12-PNG-HD.png" />
+        </div>
+        <div className="text-wrapper item">
+          <span>I-Phone</span>
+          <span>Price: $1000.00</span>
+        </div>
+        <div className="btn-wrapper item">
+          <button
+            onClick={() => {
+              props.addToCartHandler({ pice: 1000, name: "i phone 11" });
+            }}
+          >
+            Add To Cart
+          </button>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Home
+export default Home;
